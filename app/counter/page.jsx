7f43642fffc6a162a0ea50f9eef080deee59e70d
QueenPascal <deesammpa@gmail.com>
2024-06-02 13:41:19 +0200

    update value not working

--- app/counter/page.jsx
+++ app/counter/page.jsx
@@ -3,26 +3,15 @@ import React from 'react'
 
 function counterPage() {
     let number = 0
+    number +=5
 
-    const increaseNumber = () => {
-        number++
-        console.log("number increasing", number)
-    }
-    increaseNumber()
+    
 
   return (
       <div>
           
           <h1>{number}</h1>
-          {/* The next line generating error... */}
-
-          {/* The error telling us something very important */}
-          {/* onClick att can't be passed into server component */}
-          {/* nextjs components are server component in defaut, do you remeber how to change that? */}
-          {/* look at the top of this document for the anser ;) */}
-
-          {/* you'll notice that the logs now are moved to the browser side, but still the increased number is not rendered! why? */}
-          <button onClick={increaseNumber}>click</button>
+          <button className='w-50 h-30 rounded bg-pink-300' onClick={() => number += 1}>click</button>
         
       
       </div>
